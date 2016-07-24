0295901e4bea4d1f62b3bb50c47f94e901693e9d
Martin Alsinet <malsinet@gmail.com>
2016-07-24 18:27:36 -0500

    Validations: Added Email & DuplicateField

---
+++ Email.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * Email class file
+ *
+ * @category   Validations
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+
+
+namespace github\malsinet\Railway\Validations;
+
+
+/**
+ * Email class
+ *
+ * Throws an exception if the required $field is not a latitude
+ *
+ * @category   Validations
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+final class Email implements Contracts\Valid
+{
+
+    /**
+     * Previous link in the validation chain
+     *
+     * @var Contracts\Valid
+     */
+    private $origin;
+
+    /**
+     * Email field name
+     *
+     * @var string
+     */
+    private $field;
+    
+    /**
+     * Request object
+     *
+     * @var Contracts\Request
+     */
+    public $req;
+   
+    /**
+     * Class constructor
+     *
+     * @param Contracts\Valid  $origin Previous link in the validation chain
+     * @param string           $field  Required field name
+     */
+    public function __construct(Contracts\Valid $origin, $field)
+    {
+        $this->origin = $origin;
+        $this->field = $field;
+        $this->req = $origin->req;
+    }
+
+    /**
+     * Checks if the request field is an email
+     * and executes the next validation in the chain
+     *
+     * @return bool
+     */
+    public function validate()
+    {
+        $email = $this->req->get($this->field);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            throw new ValidationException("Field {$this->field} [{$email}] must be a valid email");
+        }
+        return $this->origin->validate();
+    }
+
+}
