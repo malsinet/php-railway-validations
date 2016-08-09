6fe088878472c80cad42026332b83f2a71804b22
Martin Alsinet <malsinet@gmail.com>
2016-08-09 08:34:15 -0500

    Added Date validation

---
+++ src/Date.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * Date class file
+ *
+ * @category   Validations
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+
+
+namespace github\malsinet\Railway\Validations;
+
+
+/**
+ * Date class
+ *
+ * Throws an exception if the required $field is not a valid date
+ *
+ * @category   Validations
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+final class Date implements Contracts\Valid
+{
+
+    /**
+     * Previous link in the validation chain
+     *
+     * @var Contracts\Valid
+     */
+    private $origin;
+
+    /**
+     * Date field name
+     *
+     * @var string
+     */
+    private $field;
+    
+    /**
+     * Date format
+     *
+     * @var string
+     */
+    private $format;
+    
+    /**
+     * Request object
+     *
+     * @var Contracts\Request
+     */
+    public $req;
+   
+    /**
+     * Class constructor
+     *
+     * @param Contracts\Valid  $origin Previous link in the validation chain
+     * @param string           $field  Required field name
+     */
+    public function __construct(Contracts\Valid $origin, $field, $format)
+    {
+        $this->origin = $origin;
+        $this->field = $field;
+        $this->format = $format;
+        $this->req = $origin->req;
+    }
+
+    /**
+     * Checks if the request field is a valid date
+     * and executes the next validation in the chain
+     *
+     * @return bool
+     */
+    public function validate()
+    {
+        $date = $this->req->get($this->field);
+        if (false === strtotime($date)) {
+            throw new ValidationException(
+                "Field {$this->field} [{$date}] must be a valid date"
+            );
+        }
+        if ($date != date($this->format, strtotime($date))) {
+            throw new ValidationException(
+                "Field {$this->field} [{$date}] ".
+                "does not match date format [{$this->format}]"
+            );
+        }
+        return $this->origin->validate();
+    }
+
+}
---
+++ src/Tests/DateTest.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * DateTest file
+ *
+ * @category   Tests
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+
+
+namespace github\malsinet\Railway\Validations\Tests;
+
+use PHPUnit\Framework\TestCase;
+use github\malsinet\Railway\Validations as V;
+
+
+/**
+ * DateTest class
+ *
+ * Test should throw an exception 
+ * if the required $field is not a valid date
+ *
+ * @category   Tests
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+class DateTest extends TestCase
+{
+
+	public function testEmptyFieldThrowsException()
+	{
+        $field = new V\Date(
+            new V\AlwaysValid(
+                new V\DefaultRequest(array())
+            ),
+            $field=null, $format="Y-m-d"
+        );
+        $this->expectException(V\ValidationException::class);
+        $field->validate();
+    }
+
+	public function testInvalidDateThrowsException()
+	{
+        $field = new V\Date(
+            new V\AlwaysValid(
+                new V\DefaultRequest(array("date" => "20111-43-12"))
+            ),
+            $field="date", $format="Y-m-d"
+        );
+        $this->expectException(V\ValidationException::class);
+        $field->validate();
+    }
+
+	public function testInvalidFormatThrowsException()
+	{
+        $field = new V\Date(
+            new V\AlwaysValid(
+                new V\DefaultRequest(array("date" => "2011-02-12"))
+            ),
+            $field="date", $format="Y-m-d H:i:s"
+        );
+        $this->expectException(V\ValidationException::class);
+        $field->validate();
+    }
+
+ 	public function testEmptyDateThrowsException()
+	{
+        $field = new V\Date(
+            new V\AlwaysValid(
+                new V\DefaultRequest(
+                    array("date" => "")
+                )
+            ),
+            $field="date", $format="Y-m-d"
+        );
+        $this->expectException(V\ValidationException::class);
+        $field->validate();
+    }
+
+ 	public function testValidDate()
+	{
+        $field = new V\Date(
+            new V\AlwaysValid(
+                new V\DefaultRequest(
+                    array("date" => "2016-08-10")
+                )
+            ),
+            $field="date", $format="Y-m-d" 
+        );
+        $this->assertTrue(
+            $field->validate(),
+            "Valid date should return true"
+        );
+    }
+
+}
