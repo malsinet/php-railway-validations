ebe076be8e023221a29412e8230f46331c596f53
Martin Alsinet <malsinet@gmail.com>
2016-08-09 16:38:25 -0500

    Added Exists validation

---
+++ src/Exists.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * Exists class file
+ *
+ * @category   Validations
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+
+
+namespace github\malsinet\Railway\Validations;
+
+
+/**
+ * RequiredField class
+ *
+ * Throws an exception if the entered value 
+ * does not exist in the collection
+ *
+ * @category   Validations
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+final class Exists implements Contracts\Valid
+{
+
+    /**
+     * Previous link in the validation chain
+     *
+     * @var Contracts\Valid
+     */
+    private $origin;
+
+    /**
+     * Required field name
+     *
+     * @var string
+     */
+    private $field;
+    
+    /**
+     * Unique search object
+     *
+     * @var Contracts\Find
+     */
+    private $collection;
+
+    /**
+     * Request object
+     *
+     * @var Contracts\Request
+     */
+    public $req;
+   
+    /**
+     * Class constructor
+     *
+     * @param Contracts\Valid $origin Previous link in the validation chain
+     * @param string          $field  Required field name 
+     * @param Contracts\Find  $items  Collection to be searched for duplicates
+     */
+    public function __construct($origin, $field, $items)
+    {
+        $this->origin = $origin;
+        $this->field = $field;
+        $this->items = $items;
+        $this->req = $origin->req;
+    }
+    
+    /**
+     * Checks if the request field is unique
+     * and executes the next validation in the chain
+     *
+     * @return bool
+     */
+    public function validate()
+    {
+        $value = $this->req->get($this->field);
+        if (!$this->items->find($value)) {
+            throw new ValidationException(
+                "Field {$this->field} [$value] does not exist"
+            );
+        }
+        return $this->origin->validate();
+    }
+
+
+}
---
+++ src/Tests/ExistsTest.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * ExistsTest file
+ *
+ * @category   Tests
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+
+
+namespace github\malsinet\Railway\Validations\Tests;
+
+use PHPUnit\Framework\TestCase;
+use github\malsinet\Railway\Validations as V;
+
+
+/**
+ * ExistsTest class
+ *
+ * Test should throw an exception if the required $field is not found
+ *
+ * @category   Tests
+ * @package    Railway Validations
+ * @author     Martin Alsinet <martin@alsinet.com.ar>
+ * @copyright  2016 @MartinAlsinet
+ * @license    MIT License
+ * @version    Release: 0.1.0
+ * @link       http://github.com/malsinet/railway-validations
+ */
+class ExistsTest extends TestCase
+{
+
+	public function testMissingValueThrowsException()
+	{
+        $field = new V\Exists(
+            new V\AlwaysValid(
+                new V\DefaultRequest(array("name" => "Keith"))
+            ),
+            "name",
+            new TheBeatles(array("John", "Paul", "George", "Ringo"))
+        );
+        $this->expectException(V\ValidationException::class);
+        $field->validate();
+    }
+
+	public function testExistsReturnsTrue()
+	{
+        $field = new V\Exists(
+            new V\AlwaysValid(
+                new V\DefaultRequest(array("name" => "Paul"))
+            ),
+            "name",
+            new TheBeatles(array("John", "Paul", "George", "Ringo"))
+        );
+        $this->assertTrue(
+            $field->validate(),
+            "Existing value should return true"
+        );
+    }
+
+}
+
+
+class TheBeatles implements V\Contracts\Find
+{
+
+    private $items;
+    
+    public function __construct($items)
+    {
+        $this->items = $items;
+    }
+
+    public function find($value)
+    {
+        return in_array($value, $this->items);
+    }
+
+}
